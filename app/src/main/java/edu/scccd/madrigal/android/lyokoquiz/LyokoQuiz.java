8da915255d860bcd55aae313016544762d6aaf1d
fccstudent <fresnocitystudent@gmail.com>
2018-03-13 19:55:11 -0700

    Initial commit

---
+++ app/src/main/java/edu/scccd/madrigal/android/lyokoquiz/LyokoQuiz.java
@@ -0,0 +1,13 @@
+package edu.scccd.madrigal.android.lyokoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LyokoQuiz extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lyoko_quiz);
+	}
+}
