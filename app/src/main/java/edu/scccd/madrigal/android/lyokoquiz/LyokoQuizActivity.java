7e141232675549daa95e1d0e4e94d4b750e0d2cf
fccstudent <fresnocitystudent@gmail.com>
2018-03-15 20:00:51 -0700

    update app, TODO make iterating question populator

--- app/src/main/java/edu/scccd/madrigal/android/lyokoquiz/LyokoQuizActivity.java
+++ app/src/main/java/edu/scccd/madrigal/android/lyokoquiz/LyokoQuizActivity.java
@@ -2,6 +2,7 @@ package edu.scccd.madrigal.android.lyokoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -12,23 +13,19 @@ import java.util.Random;
 
 public class LyokoQuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "LyokoQuizActivity";
+
 	private Button mTrueButton, mFalseButton, mHintButton;
 	private ImageButton mNextButton, mPrevButton, mRandomButton;
-	private TextView mQuestionTextView;
+	private TextView mQuestionTextView, mQuestionNumberView;
 
-	private Question[] mQuestionBank = new Question[]{
-			new Question(R.string.question_question1, R.string.question_hint1, false),
-			new Question(R.string.question_question2, R.string.question_hint2, true),
-			new Question(R.string.question_question3, R.string.question_hint3, false),
-			new Question(R.string.question_question4, R.string.question_hint4, true),
-			new Question(R.string.question_question5, R.string.question_hint5, false),
-			new Question(R.string.question_question6, R.string.question_hint6, true)
-	};
+	private Question[] mQuestionBank;
 
 	private int mCurrentIndex = 0;
 
 	private void updateQuestion() {
 		mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getQuestionId());
+		mQuestionNumberView.setText((mCurrentIndex+1) + "/" + mQuestionBank.length);
 	}
 
 	private void checkAnswer(boolean answer) {
@@ -43,11 +40,14 @@ public class LyokoQuizActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lyoko_quiz);
 
+		Log.d(TAG, "the sytem just called onCreate(Bundle) and life is good?");
+
 		mTrueButton = findViewById(R.id.true_button);
 		mFalseButton = findViewById(R.id.false_button);
 		mNextButton = findViewById(R.id.next_button);
 		mPrevButton = findViewById(R.id.prev_button);
 		mQuestionTextView = findViewById(R.id.question_text_view);
+		mQuestionNumberView = findViewById(R.id.question_number_view);
 		mHintButton = findViewById(R.id.hint_button);
 		mRandomButton = findViewById(R.id.random_button);
 
@@ -106,4 +106,45 @@ public class LyokoQuizActivity extends AppCompatActivity {
             }
         });
 	}
+
+	@Override
+	public void
+	onStart() {
+		super.onStart();
+		Log.d(TAG,"onStart() called");
+	}
+
+	@Override
+	public void
+	onPause() {
+		super.onPause();
+		Log.d(TAG,"onPause() called");
+	}
+
+	@Override
+	public void
+	onResume() {
+		super.onResume();
+		Log.d(TAG,"onResume() called");
+	}
+
+	@Override
+	public void
+	onStop() {
+		super.onStop();
+		Log.d(TAG,"onStop() called");
+	}
+
+	@Override
+	public void
+	onDestroy() {
+		super.onDestroy();
+		Log.d(TAG,"onDestroy() called");
+	}
+
+	public void populateQuestions()
+    {
+        Question[] mQuestionBank = new Question[];
+        for(int i = 0; i < R.in)
+    }
 }
